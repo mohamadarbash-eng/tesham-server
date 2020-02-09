d82069554eb55cc871042d3132f24a5c41ad0c4d
Arbash Mohamad <eng@mohamadarbash.com>
2020-02-09 13:34:09 +0100

    upgrade comment

--- middlewares/check-auth.js
+++ middlewares/check-auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    // TODO repair this Damn, you can send data with the request like req.dssddsds = dfffdfdf
+    // TODO repair this chaos, you can send data with the request like req.dssddsds = dfffdfdf
     try {
         const token = req.headers['authorization'].split(' ')[1];
         jwt.verify(token, process.env.JWT_KEY);
